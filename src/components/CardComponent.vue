3500c46aa3c6a97f011310b8a023c665bf998963
RickCraco <riccardocracolici43@gmail.com>
2023-11-15 16:49:50 +0100

    aggiunto template per ogni card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,9 +1,32 @@
 <template>
+    <div class="row">
+        <div class="col">
+            <img :src="img" :alt="title">
+            <div>
+                <h3>{{ title }}</h3>
+                <p>{{ originalTitle }}</p>
+                <p>{{ lingua }}</p>
+                <p>{{ voto }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'CardComponent',
+        props:{
+            title: String,
+            originalTitle: String,
+            lingua: String,
+            voto: String,
+            img: String,
+        },
+        data(){
+            return{
+
+            }
+        }
     }
 </script>
 
