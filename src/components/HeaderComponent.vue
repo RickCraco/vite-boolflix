d006553ebe053ca4c1086a0ffbb09169b4eebbd8
RickCraco <riccardocracolici43@gmail.com>
2023-11-15 16:37:54 +0100

    aggiunto bottone e input

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,11 @@
 <template>
-    
+    <header>
+        <div><h1>BOOLFLIX</h1></div>
+        <div>
+            <input type="text">
+            <button>Cerca</button>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -15,5 +21,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+    h1{
+        color: red;
+    }
 </style>
