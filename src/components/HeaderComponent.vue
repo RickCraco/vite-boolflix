929a8a80d38d517fd3c4839867a7d009ba817b98
RickCraco <riccardocracolici43@gmail.com>
2023-11-15 16:32:17 +0100

    creato componente header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+    import {store} from '../store';
+    export default {
+        name: 'HeaderComponent',
+        data(){
+            return{
+                store
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
