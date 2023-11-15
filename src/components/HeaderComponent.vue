3a146121f9c53237f18dab1760dff573f9171ab8
RickCraco <riccardocracolici43@gmail.com>
2023-11-15 16:39:57 +0100

    aggiunto stile all'header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,27 +1,31 @@
 <template>
-    <header>
-        <div><h1>BOOLFLIX</h1></div>
-        <div>
-            <input type="text">
-            <button>Cerca</button>
+    <header class="bg-black">
+        <div class="container d-flex align-items-center justify-content-between">
+            <div>
+                <h1>BOOLFLIX</h1>
+            </div>
+            <div>
+                <input type="text">
+                <button>Cerca</button>
+            </div>
         </div>
     </header>
 </template>
 
 <script>
-    import {store} from '../store';
-    export default {
-        name: 'HeaderComponent',
-        data(){
-            return{
-                store
-            }
+import { store } from '../store';
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            store
         }
     }
+}
 </script>
 
 <style lang="scss" scoped>
-    h1{
-        color: red;
-    }
+h1 {
+    color: red;
+}
 </style>
