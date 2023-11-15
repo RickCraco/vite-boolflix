f4f9f5f9432ea4a9c069f8471e9f8c31ec0a975e
RickCraco <riccardocracolici43@gmail.com>
2023-11-15 16:03:14 +0100

    aggiunti metodi per chiamata API per lista film e serie

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,32 @@
 </template>
 
 <script>
+  import axios from 'axios';
+  import { store } from './store';
+
   export default {
-    
+    name: 'App',
+    data(){
+      return{
+        store
+      }
+    },
+    methods: {
+      getMovieList(){
+        const url = store.apiUrl + store.endpoint.movie;
+        axios.get(url, store.params).then((response) =>{
+          store.movieList = response.data.results;
+          console.log(store.movieList);
+        })
+      },
+      getSeriesList(){
+        const url = store.apiUrl + store.endpoint.series;
+        axios.get(url, store.params).then((response) =>{
+          store.seriesList = response.data.results;
+          console.log(store.seriesList);
+        })
+      }
+    }
   }
 </script>
 
