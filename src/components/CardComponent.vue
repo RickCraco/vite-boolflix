00bd68b812ed37e270eed8a1cf5a41765ac5d5a3
RickCraco <riccardocracolici43@gmail.com>
2023-11-15 17:09:14 +0100

    fix

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,35 +1,31 @@
 <template>
-    <div class="row">
-        <div class="col">
-            <img :src="img" :alt="title">
-            <div>
-                <h3>{{ title }}</h3>
-                <p>{{ originalTitle }}</p>
-                <p>{{ lingua }}</p>
-                <p>{{ voto }}</p>
-            </div>
+    <div class="col-2">
+        <img :src="img" :alt="title">
+        <div>
+            <h3>{{ title }}</h3>
+            <p>{{ originalTitle }}</p>
+            <p>{{ lingua }}</p>
+            <p>{{ voto }}</p>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'CardComponent',
-        props:{
-            title: String,
-            originalTitle: String,
-            lingua: String,
-            voto: String,
-            img: String,
-        },
-        data(){
-            return{
+export default {
+    name: 'CardComponent',
+    props: {
+        title: String,
+        originalTitle: String,
+        lingua: String,
+        voto: String,
+        img: String,
+    },
+    data() {
+        return {
 
-            }
         }
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
