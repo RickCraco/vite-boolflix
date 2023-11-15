c133d3037524c0d0af082892805bae91a2b7d476
RickCraco <riccardocracolici43@gmail.com>
2023-11-15 17:09:11 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,51 @@
 <template>
-  <HeaderComponent/>
-  <CardComponent v-for="movie in store.movieList" 
-  :title="movie.title" 
-  :originalTitle="movie.original_title" 
-  :lingua="movie.original_language" 
-  :voto="movie.vote_average" 
-  :img="movie.poster_path"/>
+  <HeaderComponent />
+  <div class="container">
+    <div class="row">
+      <CardComponent v-for="movie in store.movieList" :title="movie.title" :originalTitle="movie.original_title"
+        :lingua="movie.original_language" :voto="movie.vote_average" :img="movie.poster_path" />
+    </div>
+  </div>
 </template>
 
 <script>
-  import axios from 'axios';
-  import { store } from './store';
-  import HeaderComponent from './components/HeaderComponent.vue';
-  import CardComponent from './components/CardComponent.vue';
+import axios from 'axios';
+import { store } from './store';
+import HeaderComponent from './components/HeaderComponent.vue';
+import CardComponent from './components/CardComponent.vue';
 
-  export default {
-    name: 'App',
-    components: {
+export default {
+  name: 'App',
+  components: {
     HeaderComponent,
     CardComponent
-},
-    data(){
-      return{
-        store
-      }
-    },
-    methods: {
-      getMovieList(){
-        const url = store.apiUrl + store.endpoint.movie;
-        axios.get(url, {params: store.params}).then((response) =>{
-          store.movieList = response.data.results;
-          console.log(store.movieList);
-        })
-      },
-      getSeriesList(){
-        const url = store.apiUrl + store.endpoint.series;
-        axios.get(url, {params: store.params}).then((response) =>{
-          store.seriesList = response.data.results;
-          console.log(store.seriesList);
-        })
-      }
-    },
-    created(){
+  },
+  data() {
+    return {
+      store
     }
+  },
+  methods: {
+    getMovieList() {
+      const url = store.apiUrl + store.endpoint.movie;
+      axios.get(url, { params: store.params }).then((response) => {
+        store.movieList = response.data.results;
+        console.log(store.movieList);
+      })
+    },
+    getSeriesList() {
+      const url = store.apiUrl + store.endpoint.series;
+      axios.get(url, { params: store.params }).then((response) => {
+        store.seriesList = response.data.results;
+        console.log(store.seriesList);
+      })
+    }
+  },
+  created() {
+    this.getMovieList()
+    this.getSeriesList()
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
