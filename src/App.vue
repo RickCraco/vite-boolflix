90868bca90af1e79e0fc0bdad7defdbf4bc1138c
RickCraco <riccardocracolici43@gmail.com>
2023-11-16 09:27:08 +0100

    aggiunto colore al bg dell'app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <HeaderComponent />
-  <div class="container py-3">
-    <div class="row g-2">
-      <h2>Films</h2>
-      <CardComponent v-for="movie in store.movieList" :title="movie.title" :originalTitle="movie.original_title"
-        :lingua="movie.original_language" :voto="Math.round(movie.vote_average / 2)" :img="store.imgPath + movie.poster_path" :trama="movie.overview"/>
-    </div>
-    <div class="row g-3 mt-3">
-      <h2>TV Series</h2>
-      <CardComponent v-for="serie in store.seriesList" :title="serie.name" :originalTitle="serie.original_name" :lingua="serie.original_language" 
-      :voto="Math.round(serie.vote_average / 2)" :img="store.imgPath + serie.poster_path" :trama="serie.overview"/>
+  <div class="bg-secondary">
+    <div class="container py-3">
+      <div class="row g-2">
+        <h2>Films</h2>
+        <CardComponent v-for="movie in store.movieList" :title="movie.title" :originalTitle="movie.original_title"
+          :lingua="movie.original_language" :voto="Math.round(movie.vote_average / 2)"
+          :img="store.imgPath + movie.poster_path" :trama="movie.overview" />
+      </div>
+      <div class="row g-3 mt-3">
+        <h2>TV Series</h2>
+        <CardComponent v-for="serie in store.seriesList" :title="serie.name" :originalTitle="serie.original_name"
+          :lingua="serie.original_language" :voto="Math.round(serie.vote_average / 2)"
+          :img="store.imgPath + serie.poster_path" :trama="serie.overview" />
+      </div>
     </div>
   </div>
 </template>
