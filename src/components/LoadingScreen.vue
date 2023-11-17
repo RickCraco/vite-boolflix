c0475985d3466862637dc3de502a19362b99fb5b
RickCraco <riccardocracolici43@gmail.com>
2023-11-17 14:31:39 +0100

    aggiunto v-if per la schermata iniziale

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,7 +1,15 @@
 <template>
-    <video autoplay muted>
-        <source src="/Netflix_Intro.mp4">
-    </video>
+    <div class="d-flex justify-content-center align-items-center bg-black w-100 vh-100" v-if="!store.videoFlag">
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png" alt="logo">
+        <div>
+            <button class="btn btn-danger" @click="enterBtn">ENTER</button>
+        </div>
+    </div>
+    <div v-else>
+        <video autoplay muted>
+            <source src="/Netflix_Intro.mp4">
+        </video>
+    </div>
 </template>
 
 <script>
@@ -18,10 +26,13 @@
                 setTimeout(() =>{
                     store.loadingFlag = true;
                 }, 4000)
+            },
+            enterBtn(){
+                store.videoFlag = true;
+                this.introF();
             }
         },
         created(){
-            this.introF();
         }
     }
 </script>
