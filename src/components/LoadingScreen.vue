9ffca52b6e5db4f1c0e2d68ef657bdfb7d107f24
RickCraco <riccardocracolici43@gmail.com>
2023-11-17 14:54:52 +0100

    fix

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="d-flex justify-content-center align-items-center bg-black w-100 vh-100" v-if="!store.videoFlag">
-        <img src="https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png" alt="logo">
-        <div>
-            <button class="btn btn-danger" @click="enterBtn">ENTER</button>
+    <div class="bg-black w-100 vh-100" v-if="!store.videoFlag">
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <div class="d-flex justify-content-center align-items-center mt-4 pt-4">
+                <img src="https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png" alt="logo">
+            </div>
+            <div class="text-center mt-4 pt-4">
+                <button class="btn btn-danger" @click="enterBtn">ENTER</button>
+            </div>
         </div>
     </div>
     <div v-else>
@@ -13,33 +17,33 @@
 </template>
 
 <script>
-    import { store } from '../store';
-    export default {
-        name: 'LoadingScreen',
-        data(){
-            return{
-                store
-            }
-        },
-        methods: {
-            introF(){
-                setTimeout(() =>{
-                    store.loadingFlag = true;
-                }, 4000)
-            },
-            enterBtn(){
-                store.videoFlag = true;
-                this.introF();
-            }
-        },
-        created(){
+import { store } from '../store';
+export default {
+    name: 'LoadingScreen',
+    data() {
+        return {
+            store
         }
+    },
+    methods: {
+        introF() {
+            setTimeout(() => {
+                store.loadingFlag = true;
+            }, 4000)
+        },
+        enterBtn() {
+            store.videoFlag = true;
+            this.introF();
+        }
+    },
+    created() {
     }
+}
 </script>
 
 <style lang="scss" scoped>
-    video{
-        width: 100%;
-        height: 100%;
-    }
+video {
+    width: 100%;
+    height: 100%;
+}
 </style>
