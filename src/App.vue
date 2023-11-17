6f61cd30be4a833e49d1cf9ada3c4373257ca15b
RickCraco <riccardocracolici43@gmail.com>
2023-11-17 16:01:36 +0100

    fix + aggiunte funzione per lo scroll del carosello

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
       <div class="container py-3">
         <h2 v-if="!store.film_serie_flag" class="text-white">Film in tendenza</h2>
         <h2 v-else class="text-white">Films</h2>
-        <div class="row g-2 flex-nowrap overflow-hidden">
+        <div class="row g-2 flex-nowrap overflow-hidden" ref="movies">
           <CardComponent v-for="movie in store.movieList" :title="movie.title" :originalTitle="movie.original_title"
             :lingua="movie.original_language" :voto="Math.round(movie.vote_average / 2)"
             :img="store.imgPath + movie.poster_path" :trama="movie.overview" />
         </div>
         <h2 v-if="!store.film_serie_flag" class="text-white">Serie in tendenza</h2>
         <h2 v-else class="text-white">TV Series</h2>
-        <div class="row g-3 mt-3 flex-nowrap overflow-hidden">
+        <div class="row g-3 mt-3 flex-nowrap overflow-hidden" ref="series">
           <CardComponent v-for="serie in store.seriesList" :title="serie.name" :originalTitle="serie.original_name"
             :lingua="serie.original_language" :voto="Math.round(serie.vote_average / 2)"
             :img="store.imgPath + serie.poster_path" :trama="serie.overview" />
@@ -57,6 +57,18 @@ export default {
         console.log(store.seriesList);
       })
     },
+    scrollMovieright(){
+      this.$movies.scrollBy(400, 0);
+    },
+    scrollMovieleft(){
+      this.$movies.scrollBy(-400,0);
+    },
+    scrollSeriesright(){
+      this.$series.scrollBy(400,0);
+    },
+    scrollSeriesleft(){
+      this.$series.scrollBy(-400,0);
+    }
   },
   created() {
     this.getMovieList()
