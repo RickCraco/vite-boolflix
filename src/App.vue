50a0f3e3712658a9c80437d489cb3ccb59bba280
RickCraco <riccardocracolici43@gmail.com>
2023-11-17 15:19:03 +0100

    cambiato colore h2

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@
     <div class="bg-secondary">
       <div class="container py-3">
         <div class="row g-2">
-          <h2 v-if="!store.film_serie_flag">Film in tendenza</h2>
-          <h2 v-else>Films</h2>
+          <h2 v-if="!store.film_serie_flag" class="text-white">Film in tendenza</h2>
+          <h2 v-else class="text-white">Films</h2>
           <CardComponent v-for="movie in store.movieList" :title="movie.title" :originalTitle="movie.original_title"
             :lingua="movie.original_language" :voto="Math.round(movie.vote_average / 2)"
             :img="store.imgPath + movie.poster_path" :trama="movie.overview" />
         </div>
         <div class="row g-3 mt-3">
-          <h2 v-if="!store.film_serie_flag">Serie in tendenza</h2>
-          <h2 v-else>TV Series</h2>
+          <h2 v-if="!store.film_serie_flag" class="text-white">Serie in tendenza</h2>
+          <h2 v-else class="text-white">TV Series</h2>
           <CardComponent v-for="serie in store.seriesList" :title="serie.name" :originalTitle="serie.original_name"
             :lingua="serie.original_language" :voto="Math.round(serie.vote_average / 2)"
             :img="store.imgPath + serie.poster_path" :trama="serie.overview" />
