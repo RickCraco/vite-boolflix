ab4143d5448f14b724b2abfdbb47246a0dfa00d2
RickCraco <riccardocracolici43@gmail.com>
2023-11-20 12:18:55 +0100

    fixed series search

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -50,14 +50,14 @@ export default {
             if (this.searchString === '') {
                 const url = store.trendingTvUrl;
                 axios.get(url + store.params.api_key).then((response) => {
-                    store.seriesList = response.data.results;
+                    store.filterS = response.data.results;
                 })
             } else {
                 store.film_serie_flag = true;
                 store.params.query = this.searchString;
                 const url = store.apiUrl + store.endpoint.series;
                 axios.get(url, { params: store.params }).then((response) => {
-                    store.seriesList = response.data.results;
+                    store.filterS = response.data.results;
                 })
             }
         },
